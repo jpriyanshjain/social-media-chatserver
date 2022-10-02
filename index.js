c3e970350f65998dcef6255dc8a48d0963bda337
jpriyanshjain <jpriyanshjain@gmail.com>
2022-10-02 21:09:29 +0530

    chat server added

---
+++ index.js
@@ -0,0 +1,37 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const io = require("socket.io")(process.env.PORT, {
+  cors: {
+    origin: process.env.URL,
+  },
+});
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+  // add new User
+  socket.on("new-user-add", (newUserId) => {
+    if (!activeUsers.some((user) => user.userId === newUserId)) {
+      activeUsers.push({
+        userId: newUserId,
+        socketId: socket.id,
+      });
+      io.emit("get-users", activeUsers);
+    }
+  });
+
+  //send message
+  socket.on("send-message", (data) => {
+    const { receiverId } = data;
+    const user = activeUsers.find((user) => user.userId === receiverId);
+    if (user) {
+      io.to(user.socketId).emit("recieve-message", data);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("get-users", activeUsers);
+  });
+});
